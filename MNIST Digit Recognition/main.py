14b8e399ba54e9244f17271c0421fd7b19d687b8
anshulg03 <anshulgupta301@gmail.com>
2017-04-16 19:01:34 +0530

    MNIST

--- MNIST Digit Recognition/main.py
+++ MNIST Digit Recognition/main.py
@@ -22,7 +22,7 @@ def load(path_img, path_lbl):
 
   images = []
   for i in range(size): images.append([0] * rows * cols)
-  for i in range(size): images[i] = np.array(image_data[i * rows * cols:(i + 1) * rows * cols]).reshape(28,28)[::3,::3].reshape(-1)
+  for i in range(size): images[i] = np.array(image_data[i * rows * cols:(i + 1) * rows * cols]).reshape(28,28)[::,::].reshape(-1)
   return pd.DataFrame(images), pd.Series(labels)
 
 def peekData(X_train):
@@ -34,42 +34,46 @@ def peekData(X_train):
   for col in range(5):
     for row in range(10):
       plt.subplot(5, 10, cnt + 1)
-      plt.imshow(X_train.ix[cnt,:].values.reshape(10,10), cmap=plt.cm.gray_r, interpolation='nearest')
+      plt.imshow(X_train.ix[cnt,:].values.reshape(28,28), cmap=plt.cm.gray_r, interpolation='nearest')
       plt.axis('off')
       cnt += 1
   fig.set_tight_layout(True)
   plt.show()
+
+# To check for a random number
+def pred_check(a):
+    true_1000th_test_value = y_test[a]
+    
+    print ("1000th test label: ", true_1000th_test_value)
+    
+    guess_1000th_test_value = model.predict(X_test.iloc[[a]])
+    print ("1000th test prediction: ", guess_1000th_test_value)
+    plt.imshow(X_test.ix[a,:].values.reshape(28,28), cmap=plt.cm.gray_r, interpolation='nearest')
+
+
   
 
 dir = 'C:/Users/anshangu/Documents/GitHub/Python_LogisticReg/DataScience-with-Python/MNIST Digit Recognition/'
-
 X_train, y_train = load(dir + 'train-images-idx3-ubyte.gz', dir + 'train-labels-idx1-ubyte.gz')
-
 X_test, y_test = load(dir + 't10k-images-idx3-ubyte.gz', dir + 't10k-labels-idx1-ubyte.gz')
 
-
-X_train = X_train.ix[:39999]
-y_train = y_train[:40000]
 
 peekData(X_train)
 
 
-print('Training SVC Classifier...')
-from sklearn.svm import SVC
-
-
-model = SVC(kernel = 'linear', cache_size = 7000)
+print('Training KNN Classifier...')
+from sklearn.neighbors import KNeighborsClassifier
+model = KNeighborsClassifier(n_neighbors=7)
 model.fit(X_train, y_train)
-
 print('Scoring model....')
-score = model.score(X_test,y_test)
+score = model.score(X_test, y_test)
+print('KNN Score \n', score)
 
-print('Score \n', score)
 
-
-true_1000th_test_value = y_test[1000]
-
-print ("1000th test label: ", true_1000th_test_value)
-
-guess_1000th_test_value = model.predict(X_test.iloc[[1000]])
-print ("1000th test prediction: ", guess_1000th_test_value)
+#print('Training SVC Classifier...')
+#from sklearn.svm import SVC
+#model = SVC(kernel = 'linear', cache_size = 7000)
+#model.fit(X_train, y_train)
+#print('Scoring model....')
+#score = model.score(X_test,y_test)
+#print('SVC Score \n', score)
